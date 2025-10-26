c689af9197b54976f663b36bce440f4e892eca3a
moxxxiiee <cristianagnesiusayub@gmail.com>
2025-10-26 14:46:29 +0700

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { RouterModule, Routes } from '@angular/router';
+import { Dashboard} from './dashboard/dashboard';
+import { Login } from './login/login';
+import { NgModule } from '@angular/core';
+import { Admin } from './admin/admin';
+
+export const routes: Routes = [
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "admin", component: Admin },
+  { path: "dashboard", component: Dashboard },
+  { path: "login", component: Login }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutes { }
