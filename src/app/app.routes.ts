08059a2fffb1c10ba4ae76f8e758c842eb83b4b2
moxxxiiee <cristianagnesiusayub@gmail.com>
2025-12-07 15:14:24 +0700

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { Dashboard2 } from './dashboard2/dashboard2';
 import { Dashboard3 } from './dashboard3/dashboard3'; 
 import { Mahasiswa } from './mahasiswa/mahasiswa';  
 import { otentikasiGuard } from './otentikasi-guard';
+import { Logout } from './logout/logout';
 
 export const routes: Routes = [
   { path: "", redirectTo: "login", pathMatch: "full" },
@@ -15,6 +16,7 @@ export const routes: Routes = [
   { path: "dashboard2", component: Dashboard2, canActivate: [otentikasiGuard] },
   { path: "dashboard3", component: Dashboard3, canActivate: [otentikasiGuard] },  
   { path: "login", component: Login },
+  { path: "logout", component: Logout },
   { path: "mahasiswa", component: Mahasiswa, canActivate: [otentikasiGuard] }
 ];
 
--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -1,14 +1,33 @@
-import { Component, Input } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-sidebar',
-  imports : [ RouterModule ],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './sidebar.html',
-  styleUrl: './sidebar.css',
+  styleUrl: './sidebar.css'
 })
-export class Sidebar {
+export class Sidebar implements OnInit {
 
   @Input() moduleName: string = "";
+  username: string = "";
+
+  constructor(private cookieService: CookieService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.username = this.cookieService.get('userId');
+  }
+
+  setDarkMode() {
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('theme', 'dark');
+  }
+
+  setLightMode() {
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('theme', 'light');
+  }
 
 }
