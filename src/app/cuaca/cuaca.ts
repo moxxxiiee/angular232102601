a965f25bfcac095082e4fd40f60f58ad06ba4cfa
moxxxiiee <cristianagnesiusayub@gmail.com>
2025-12-15 11:08:36 +0700

    commit

---
+++ src/app/cuaca/cuaca.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cuaca',
+  imports: [],
+  templateUrl: './cuaca.html',
+  styleUrl: './cuaca.css',
+})
+export class Cuaca {
+
+}
